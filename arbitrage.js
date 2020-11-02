fe08ceeeb8667a4f712f1750fba230477323bda0
danwez <danwez@mail.ru>
2020-11-02 16:26:50 +0300

    Update arbitrage.js

--- arbitrage.js
+++ arbitrage.js
@@ -7,12 +7,17 @@ const WebSocket = require('ws')
 const Arbitrage = function(){
 
     this.ticketArr = {}
-    this.dopSymbols = ['BTC','ETH','IDRT','DAI','PAX','BCH','ZAR','AUD','NGN','EUR','ADA','LTC']
+    this.dopSymbols = ['BTC','ETH','LTC','TRX','EOS','SXP','ADA','BCH','XRP','XMR','OCEAN','ATOM']
     this.quantity = 1.7
+    this.usdt = 50
+    this.fd = null
+    this.time = 5
+    this.tradePlan = null
 
     this.init= function(){
         let self = this
 
+        this.fd = fs.openSync('log.txt','a')
         //установка конфигурации
         for(let i in config)
             this[i] = config[i]
@@ -25,6 +30,7 @@ const Arbitrage = function(){
         
         //this.getMyBalances(true)
         
+       // this.log(JSON.stringify(this))
         this.startTrade()
         //this.accountSocket()
 
@@ -32,6 +38,12 @@ const Arbitrage = function(){
 
     this.startTrade = () => {
         this.getTickerArr();
+    }
+
+    this.log=function(message){
+        let now = new Date()
+        let date = now.getHours()+':'+now.getMinutes()+':'+now.getSeconds()
+        fs.writeFile('log.txt', '\n\r'+date+' '+ message, { flag: 'a' }, (err) => {console.log(err)})
     }
 
     this.getTickerArr = function(callback=function(){}){
@@ -44,7 +56,7 @@ const Arbitrage = function(){
             for(let i in result)
                 self.ticketArr[result[i].s] = result[i]
             
-            setTimeout(()=>socket.close(),10000)
+            setTimeout(()=>socket.close(),self.time*1000)
             
         };
         socket.onclose = function(e){
@@ -60,8 +72,21 @@ const Arbitrage = function(){
     }
 
     this.checkSymbols = (ticketArr) => {
+        this.tradePlan = null
+        let price = 0
+        let price1 = 0
+        let price2 = 0
+        // Сколько мжно купить BNB
+        for (let a in ticketArr){
+            if(ticketArr[a].s == this.symbol){
+                this.quantity = Math.floor(this.usdt / ticketArr[a].a * 1000)/1000
+                price = ticketArr[a].a
+            }
+        }
+
         console.log('Виртуально мы имеем '+this.quantity+' '+this.tradeSym)
         let etalone = 0
+
         for (let a in ticketArr){
             if(ticketArr[a].s == this.symbol){
                 etalone = ticketArr[a].b * this.quantity * 0.999
@@ -73,31 +98,83 @@ const Arbitrage = function(){
         for(let i in this.dopSymbols){
             let trans1 = 0
             let trans2 = 0
+            
+            let symbol1 = ''
+            let symbol2 = ''
+            let direction1 = 0
+            let direction2 = 0
+            // если купить промежуточную валюту
             for (let a in ticketArr){
                 //console.log(ticketArr[a])
                 if(ticketArr[a].s!=undefined && ticketArr[a].s.indexOf(this.tradeSym) > -1 && ticketArr[a].s.indexOf(this.dopSymbols[i]) > -1){
-                    if(ticketArr[a].s.indexOf(this.tradeSym)>1)
-                        trans1 = this.quantity / ticketArr[a].b
-                    else
+                    if(ticketArr[a].s.indexOf(this.tradeSym)>1){
+                        trans1 = this.quantity / ticketArr[a].a
+                        price1 = ticketArr[a].a
+                    }                        
+                    else{
                         trans1 = this.quantity * ticketArr[a].b
+                        price1 = ticketArr[a].b
+                        direction1 = 1
+                    }
+                        
+                    symbol1 = ticketArr[a].s
+                    
                     //console.log('trans1',ticketArr[a])
                 }
             }
+            // если продать промежуточную валюту
             for (let a in ticketArr){
                 //console.log(ticketArr[a].s,' ',ticketArr[a].s.replace(this.baseSym,''))
                 if(ticketArr[a].s.replace(this.baseSym,'') == this.dopSymbols[i]){
-                    if(ticketArr[a].s.indexOf(this.baseSym)>1)
+                    if(ticketArr[a].s.indexOf(this.baseSym)>1){
                         trans2 = trans1 * ticketArr[a].a
-                    else
-                        trans2 = trans1 / ticketArr[a].a
+                        price2 = ticketArr[a].a
+                        direction2 =1
+                    }
+                        
+                    else{
+                        trans2 = trans1 / ticketArr[a].b
+                        price2 = ticketArr[a].b
+                        
+                    }
+                        
                     //trans2 = trans1 * ticketArr[a].a
                     //console.log('trans2',ticketArr[a])
+                    symbol2 = ticketArr[a].s
+                    
                 }    
                     
             }
             console.log('Купить '+trans1 +' '+ this.dopSymbols[i]+' и получить '+ trans2*0.998 +' '+this.baseSym + ' выгода '+(trans2*0.998 - etalone))
-        }
+            if(trans2*0.998 - etalone > 0){
+                this.tradePlan = [
+                    {
+                        symbol : this.symbol,
+                        price : price,
+                        quantity: this.quantity,
+                        buy : true,
+                        compleat: false
+                    },
+                    {
+                        symbol : symbol1,
+                        price : price1,
+                        quantity: trans1,
+                        buy : direction1 == 0,
+                        compleat: false
+                    },
+                    {
+                        symbol : symbol2,
+                        price : price2,
+                        quantity: trans1,
+                        buy : direction2 == 0,
+                        compleat: false
+                    }
+                ]
+            }
 
+            
+        }
+        console.log(this.tradePlan)
     }
 }
 
